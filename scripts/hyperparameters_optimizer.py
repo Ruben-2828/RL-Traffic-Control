cdae7642b3032ef4629c3a535394d3d7b8e6b634
LilQuacky <andreafalbo46@gmail.com>
2024-02-08 14:37:04 +0100

    Refactor: fix some text, truncate after 5 decimal, well formatted output

--- scripts/hyperparameters_optimizer.py
+++ scripts/hyperparameters_optimizer.py
@@ -7,16 +7,16 @@ import glob
 import numpy as np
 
 def objective(trial):
-    # Define the hyperparameters to be optimized
-    alpha = trial.suggest_float('alpha', 0.0001, 0.1)
-    gamma = trial.suggest_float('gamma', 0.75, 0.999)
-    decay = trial.suggest_float('decay', 0.9, 0.9999)
+    # Hyperparameters to be optimized
+    alpha_search = trial.suggest_float('alpha', 0.0001, 0.1)
+    gamma_search = trial.suggest_float('gamma', 0.75, 0.999)
+    decay_search = trial.suggest_float('decay', 0.9, 0.9999)
 
-    # Run simulation with given hyperparameters
+    # Run simulation 
     args = argparse.Namespace(
-        alpha=alpha,
-        gamma=gamma,
-        decay=decay,
+        alpha=alpha_search,
+        gamma=gamma_search,
+        decay=decay_search,
         fixed=False,
         seconds=10000,
         route="big-intersection/BI_50_test.rou.xml",
@@ -29,6 +29,7 @@ def objective(trial):
     system_total_stoppeds = []
     system_total_waiting_times = []
 
+    # Extract the metrics from the YAML files
     for result in results:
         system_mean_speeds.append(result['system_mean_speed'])
         system_mean_waiting_times.append(result['system_mean_waiting_time'])
@@ -39,10 +40,9 @@ def objective(trial):
     return np.mean(system_mean_speeds), -np.mean(system_mean_waiting_times), -np.mean(system_total_stoppeds), -np.mean(system_total_waiting_times)
 
 def run_simulation(args):
-    # Run your simulation script with given arguments
     subprocess.run(['python', 'main.py', '-a', str(args.alpha), '-g', str(args.gamma), '-d', str(args.decay), '-fixed', str(args.fixed), '-s', str(args.seconds), '-route', str(args.route), '-runs', str(args.runs)])
 
-    # Extract the last iteration values from the YAML file
+    # Extract from YAML files
     yaml_output_folder = "output/yaml"
     yaml_files = glob.glob(os.path.join(yaml_output_folder, "values_BI_QL_conn0_ep*.yaml"))
 
@@ -55,13 +55,14 @@ def run_simulation(args):
     return all_last_iteration_values
 
 if __name__ == '__main__':
-    # Create an Optuna study and optimize the objective function
     study = optuna.create_study(directions=['maximize', 'minimize', 'minimize', 'minimize'])
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective, n_trials=10)
 
     best_trials = study.best_trials
 
-    print('All trials:')
-    for trial in best_trials:
-        print(f'Hyperparameters: {trial.params}')
-        print(f'Metrics: {trial.values}')
+    print('All best trials:')
+    # Print the best trials truncated to 5 decimal places for readability reasons
+    for idx, best_trial in enumerate(best_trials, start=1):
+        print('Hyperparameters:', {key: round(value, 5) for key, value in best_trial.params.items()})
+        print('Metrics:', {key: round(value, 5) for key, value in dict(zip(['mean_system_mean_speed', 'mean_negative_system_mean_waiting_time', 'mean_negative_system_total_stopped', 'mean_negative_system_total_waiting_time'], best_trial.values)).items()})
+
