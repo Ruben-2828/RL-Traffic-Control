274bdeca347f2bdc2bbb909b99313029d5f1efe2
LilQuacky <andreafalbo46@gmail.com>
2024-02-13 15:08:31 +0100

    Refactor: sarsa agent

--- scripts/agents/sarsa_agent.py
+++ scripts/agents/sarsa_agent.py
@@ -5,18 +5,22 @@ from sumo_rl import SumoEnvironment
 from linear_rl.true_online_sarsa import TrueOnlineSarsaLambda
 from scripts.agents.learning_agent import LearningAgent
 
+#Converts the input to a numpy array if it isn't already
+def to_numpy_array(x):
+    return np.array(x) if isinstance(x, tuple) else x
+
 class SarsaAgent(LearningAgent):
     
     def __init__(self, config: dict, env: SumoEnvironment, name: str):
         """
         Q-Learning Agent constructor
-        :param config: dict containing the configuration of the QL agent
+        :param config: dict containing the configuration of the SARSA agent
         :param env: Sumo Environment object
         """
         super().__init__(config, env, name)
 
+    #Initialize the agent with the parameters specified in the constructor
     def init_agent(self):
-        
         self.agent = TrueOnlineSarsaLambda(
             state_space=self.env.observation_space,
             action_space=self.env.action_space,
@@ -25,40 +29,32 @@ class SarsaAgent(LearningAgent):
             epsilon=self.config['Epsilon'],
             fourier_order=self.config['FourierOrder'],
             lamb=self.config['Lambda']
-    )
+        )
 
-    
+    #Learn or test the agent in the provided environment
     def run(self, env: SumoEnvironment, learn: bool, out_path: str) -> None:
         if self.agent is None:
             self.init_agent()
 
         for curr_run in range(self.config['Runs']):
-            obs, info = env.reset()
+            obs, _ = env.reset()
             terminated, truncated = False, False
             
             while not (terminated or truncated):
                 action = self.agent.act(obs)
-                next_obs, reward, terminated, truncated, info = env.step(action=action)
+                next_obs, reward, terminated, truncated, _ = env.step(action=action)
                 
-                if isinstance(obs, tuple):  
-                    obs_array = np.array(obs) 
-                else:
-                    obs_array = obs
-                    
-                if isinstance(next_obs, tuple):  
-                    next_obs_array = np.array(next_obs)  
-                else:
-                    next_obs_array = next_obs
+                obs_array = to_numpy_array(obs)
+                next_obs_array = to_numpy_array(next_obs)
                 
                 if learn:
                     self.agent.learn(state=obs_array, action=action, reward=reward, next_state=next_obs_array, done=terminated)
 
                 obs = next_obs
 
-            out_file = os.path.join(out_path, self.name, self.name)
+            out_file = os.path.join(out_path, f"{self.name}/{self.name}")
             env.save_csv(out_file, curr_run)
             env.reset()
-
 
     def save(self) -> None:
         pass
